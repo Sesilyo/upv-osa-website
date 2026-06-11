94a40520f644055de4ab70093a8af7e4ec659a0c
Sesilyo <c.sethleander@gmail.com>
2026-06-11 19:42:20 +0800

    chore: add global constants and update layout styling - add global constant variables for project-wide use - refactor and update styles for index page and navbar

--- scripts/components/headerComp.js
+++ scripts/components/headerComp.js
@@ -2,11 +2,13 @@
 // the header component injected via JavaScript
 
 export function injectHeader() {
-    const headerElem = document.querySelector('header');
+    const headerElem = document.getElementById('navbar-container');
     const navBar = `
         <div id="main-navbar">
             <a href="#" class="main-navbar-links active">HOME</a>
-            <a href="#" class="main-navbar-links active">ABOUT</a>
+            <a href="#" class="main-navbar-links">SERVICES</a>
+            <a href="#" class="main-navbar-links">ABOUT</a>
+            <a href="#" class="main-navbar-links">FACULTY & STAFF</a>
         </div>
     `;
 
